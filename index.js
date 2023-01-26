293ccff88af4fe2a3f6c43fdf8e7baef3e27da85
Gagan Tayal <gagantayal2003@gmail.com>
2023-01-26 23:31:45 +0530

    full

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var UserAPI = require('./api/user');
 const cors= require('cors')
 var app = express();
-app.use(cors)
+app.use(cors())
 app.use(express.json());
 app.use('/',UserAPI);
 app.listen(8000, ()=>{console.log('Backend Running')});
